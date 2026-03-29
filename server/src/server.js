ab37a8d71b16d50e94033a7798c9de4c82624454
Lyon-Athaly <jonathanerhandoyo18@gmail.com>
2026-03-29 22:35:35 +0700

    Add cors middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,16 @@
 import "dotenv/config";
 import express from "express";
 import menuRouter from "./routes/menuRoute.js";
+import cors from "cors";
 
 const app = express();
+const port = process.env.PORT;
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/uploads", express.static("src/uploads"));
 app.use("/menu", menuRouter);
 
-const port = process.env.PORT;
+
 app.listen(port, () => {console.log(`${port}`);});
